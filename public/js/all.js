fe4c1cc98ce806b57399743a73b86ac3a0e728fc
Vero Soto <verosoto1234@gmail.com>
2017-01-09 20:46:09 -0300

    Final-Final project

--- public/js/all.js
+++ public/js/all.js
@@ -1,37 +1,4 @@
 $(document).ready(function(){
-    
-    /*var menuDesktop = $(".olapic-nav [data-link]"),
-        menuMobile = $(".olapic-nav .mobile_options"),
-        images = $("[data-img]"),
-        category = "",
-        menuMobileText = $(".menu_mobile .menu_mobile_text");
-
-    menuDesktop.on("click", function(){
-        category = $(this).data("link");
-        menuMobile.find("option").filter(function() {
-            return $(this).val() === category; 
-        }).prop('selected', true);
-
-        menuMobile.trigger("change");
-    });
-
-    menuMobile.on("change", function(){
-        var text = menuMobile.find("option:selected").text();
-        category = menuMobile.val();
-        
-        activarImgs(category);
-        menuMobileText.text(text);
-    });
-    
-    function activarImgs(c){
-        images
-            .hide()
-            .filter('[data-img="' + c + '"]').show();
-    }*/
-
-    /*$(".mobile_options").val(function(){
-        $("[data-name]").trigger( "click" );
-    });*/
 
     var menuMobile = $(".olapic-nav_mobile .mobile_options");
     menuMobileText = $(".menu_mobile .menu_mobile_text");
